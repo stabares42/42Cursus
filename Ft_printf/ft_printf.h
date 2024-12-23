06b13cef4015401dcf848e9bbaded78a7e96de42
Santiago Tabares Agudelo <stabares@c3r6s6.42urduliz.com>
2024-12-23 18:15:32 +0100

    231224 URDULIZ

---
+++ Ft_printf/ft_printf.h
@@ -0,0 +1,11 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int		ft_printf(char const *, ...);
+
+#endif
