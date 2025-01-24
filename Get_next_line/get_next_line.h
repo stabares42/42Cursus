4b33d7fb5b8f6ea13ca3d175a11d97bb3f2eff59
Santiago Tabares Agudelo <stabares@c3r7s6.42urduliz.com>
2025-01-24 17:54:13 +0100

    240125 URDULIZ

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,7 +6,19 @@
 /*   By: stabares <stabares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:02:55 by stabares          #+#    #+#             */
-/*   Updated: 2025/01/22 18:02:56 by stabares         ###   ########.fr       */
+/*   Updated: 2025/01/24 17:27:50 by stabares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 42
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+char	*get_next_line(int fd);
+
+#endif
